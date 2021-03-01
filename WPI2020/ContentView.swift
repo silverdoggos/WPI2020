b9a58931915d51dc36bd72d5af9ce6c13fc6114c
Artem Shishkin <shishkinart@icloud.com>
2021-03-01 21:28:52 +0300

    Initial Commit

---
+++ WPI2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WPI2020
+//
+//  Created by Artem Shishkin on 01.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WPI2020/WPI2020App.swift
@@ -0,0 +1,17 @@
+//
+//  WPI2020App.swift
+//  WPI2020
+//
+//  Created by Artem Shishkin on 01.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct WPI2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
